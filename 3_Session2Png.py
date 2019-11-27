99a1c7fe1d697f8aa5d9589aea4b916ce138c9e0
蔡静轩 <38127120+cjx1016@users.noreply.github.com>
2019-11-27 18:30:53 +0800

    Update 3_Session2Png.py

--- 3_Session2Png.py
+++ 3_Session2Png.py
@@ -2,7 +2,7 @@
 #   Wei Wang (ww8137@mail.ustc.edu.cn)
 # CONTRIBUTOR:
 #   David Lu (yungshenglu1994@gmail.com)
-#
+#   bit_cjx (bit_cjx@163.com)
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file, You
 # can obtain one at http://mozilla.org/MPL/2.0/.
